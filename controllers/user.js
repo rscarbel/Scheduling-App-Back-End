5e3c8a9846c6420c1326ba8a0ee1d1f4c4618fde
Ryan Scarbel <ryanscarbel@gmail.com>
2021-11-03 15:40:55 -0400

    Added route for creating user

--- controllers/user.js
+++ controllers/user.js
@@ -4,15 +4,19 @@ const bcrypt = require('bcrypt');
 const User = require('../models/user');
 const isAuthenticated = require ('../bin/isAuthenticated');
 
+const setSessionProperties = (model) => {
+  req.session.user = model._id;
+  req.session.company = model.companyName;
+  req.session.firstName = model.firstName;
+  req.session.lastName = model.lastName;
+}
+
 router.post ('/login', (req,res) => {
   User.findOne({ username: req.body.username }, (err, foundUser) => {
     if (foundUser) {
       const passwordMatched = bcrypt.compareSync(req.body.password,foundUser.password);
       if (passwordMatched) {
-        req.session.user = foundUser._id;
-        req.session.company = foundUser.companyName;
-        req.session.firstName = foundUser.firstName;
-        req.session.lastName = foundUser.lastName;
+        setSessionProperties(foundUser)
       } else {
         res.send('Invalid username or password')
       }
@@ -22,3 +26,14 @@ router.post ('/login', (req,res) => {
   })
 });
 
+router.post('/signup',(req,res) => {
+  req.body.password = bcrypt.hashSync(req.body.password,bcrypt.genSalt(10));
+
+  User.create (req.body, (err, newUser) => {
+    if (err) {
+      res.send(err.code === 11000 ? 'Email must be unique' : 'Something went wrong');
+    } else {
+      setSessionProperties(newUser)
+    }
+  })
+})
