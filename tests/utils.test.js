a93791fe4378c2464463e24fe123bc15569cca61
Ryan Scarbel <ryanscarbel@gmail.com>
2021-11-04 12:54:33 -0400

    Added tests for more edge cases

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -2,14 +2,19 @@ const { timestamp } = require('../bin/utils');
 
 describe('timestamp', () => {
 
-  const date1 = new Date('December 17, 1995 03:24:00');
-  const date2 = new Date('1995-12-17T03:24:00')
+  const date1 = new Date('December 17, 1994 10:24:00');
+  const date2 = new Date('January 17, 1994 10:24:00');
+  const date3 = new Date('January 1, 1994 10:24:00');
+  const date4 = new Date('January 1, 1994 1:24:00');
+  const date5 = new Date('January 1, 1994 1:1:00');
+  const date6 = new Date('January 1, 1994 13:1:00');
+  const date7 = new Date('January 1, 1994 0:0:00');
 
-  test ('It should return a string with no arguments', () => {
+  test ('It should return a string when NOT passed an arguments', () => {
     expect(typeof timestamp()).toBe('string');
   });
 
-  test ('It should return a string with an argument', () => {
+  test ('It should return a string when passed an argument', () => {
     expect(typeof timestamp(date1)).toBe('string');
   });
 
@@ -18,11 +23,31 @@ describe('timestamp', () => {
   });
 
   test('It should accurately produce a readable timestamp', () => {
-    expect (timestamp(date1)).toBe('12/17/1995 03:24 am');
+    expect (timestamp(date1)).toBe('12/17/1994 10:24 am');
   });
 
-  test('It should produce consistent results when equivalent dates are calculated', () => {
-    expect (timestamp(date1)).toBe(timestamp(date2));
+  test('Single digit months should be converted to double-digits', () => {
+    expect (timestamp(date2)).toBe('01/17/1994 10:24 am');
+  });
+
+  test('Single digit days should be converted to double-digits', () => {
+    expect (timestamp(date3)).toBe('01/01/1994 10:24 am');
+  });
+
+  test('Single digit hours should be converted to double-digits', () => {
+    expect (timestamp(date4)).toBe('01/01/1994 01:24 am');
+  });
+
+  test('Single digit minutes should be converted to double-digits', () => {
+    expect (timestamp(date5)).toBe('01/01/1994 01:01 am');
+  });
+
+  test('afternoon should say pm', () => {
+    expect (timestamp(date6)).toBe('01/01/1994 01:01 pm');
+  });
+
+  test('00 as the hour should convert to 12pm', () => {
+    expect (timestamp(date7)).toBe('01/01/1994 12:00 am');
   });
 
 });
