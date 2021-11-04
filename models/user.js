057ea31f7350864c8889089d426ea361eb2fff92
Ryan Scarbel <ryanscarbel@gmail.com>
2021-11-04 08:23:07 -0400

    Added more fields & reverted name to user

--- models/user.js
+++ models/user.js
@@ -2,11 +2,16 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  email: {type: String, unique: true, required: true},
+  email: {type: String, unique: true, required: true, maxLength: 320},
   password: {type: String, required: true},
-  firstName: {type: String, required: true},
-  lastName: {type: String, required: true},
-  companyName: {type: String, required: true}
+  firstName: {type: String, required: true, maxLength: 320},
+  lastName: {type: String, required: true, maxLength: 320},
+  companyName: {type: String, required: true, maxLength: 320},
+  position: {type: String, maxLength: 320},
+  phone: {type: Number},
+  availableHours: Array,
+  loginHistory: {type:Array},
+  isAdmin: {type: Boolean, default: false, immutable: true}
 }, {
   timestamps: true
 });
