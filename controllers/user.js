28af661dd9128f0a0388b0409cb6bebcd9ef81ae
Ryan Scarbel <ryanscarbel@gmail.com>
2021-11-04 08:23:39 -0400

    Added route for admin signup and regular signup
    also added function to insert timestamp in user document when they login

--- controllers/user.js
+++ controllers/user.js
@@ -2,13 +2,14 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const User = require('../models/user');
-const isAuthenticated = require ('../bin/isAuthenticated');
+const utils = require('../bin/utils')
 
 const setSessionProperties = (session, model) => {
   session.user = model._id;
   session.company = model.companyName;
   session.firstName = model.firstName;
   session.lastName = model.lastName;
+  session.isAdmin = model.isAdmin;
 }
 
 router.post ('/login', (req,res) => {
@@ -16,8 +17,13 @@ router.post ('/login', (req,res) => {
     if (foundUser) {
       const passwordMatched = bcrypt.compareSync(req.body.password, foundUser.password);
       if (passwordMatched) {
-        setSessionProperties(req.session, foundUser)
-        res.send({success: 'successfully logged in'})
+        setSessionProperties(req.session, foundUser);
+        foundUser.loginHistory.push(utils.currentTimestamp())
+        foundUser.save()
+        res.send({
+          success: 'successfully logged in',
+          session: req.session
+        })
       } else {
         res.send({error: 'Invalid username or password'})
       }
@@ -27,12 +33,10 @@ router.post ('/login', (req,res) => {
   })
 });
 
-router.post('/signup',(req,res) => {
-
+const createAccount = (req,res) => {
   if (req.body.password) {
     req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
   }
-
   User.create (req.body, (err, newUser) => {
     if (err) {
       res.send(err.code === 11000 ? {error: 'Email must be unique'} : {error: 'Something went wrong.'});
@@ -41,7 +45,14 @@ router.post('/signup',(req,res) => {
       res.send('Account successfully created.')
     }
   })
-})
+}
+
+router.post('/admin/signup',(req,res) => {
+  req.body.isAdmin = true;
+  createAccount(req,res);
+});
+
+router.post('/signup',(req,res) => createAccount(req,res));
 
 router.get('/logout',(req,res) => {
   if (req.session.user) {
