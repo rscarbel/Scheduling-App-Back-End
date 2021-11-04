b73154eacef153eff8324d71f2347bb5c0baab24
Ryan Scarbel <ryanscarbel@gmail.com>
2021-11-04 13:33:57 -0400

    Added Available hours field

--- models/user.js
+++ models/user.js
@@ -9,7 +9,23 @@ const userSchema = new Schema({
   companyName: {type: String, required: true, maxLength: 320},
   position: {type: String, maxLength: 320},
   phone: {type: Number},
-  availableHours: Array,
+  /*
+  Each index of the available hours corrosponds with a day of the week (sunday-saturday)
+
+  Each "day" is comprised of a sub array with two numbers, each between 0 and 1440.
+
+  The numbers corrospond to minutes in a day.
+  The first number is the starting availablility time, and the second number is the ending availability time.
+  */
+  availableHours: [
+    [{type: Number, min:0, max:1440},{type: Number, min:0, max:1440}],
+    [{type: Number, min:0, max:1440},{type: Number, min:0, max:1440}],
+    [{type: Number, min:0, max:1440},{type: Number, min:0, max:1440}],
+    [{type: Number, min:0, max:1440},{type: Number, min:0, max:1440}],
+    [{type: Number, min:0, max:1440},{type: Number, min:0, max:1440}],
+    [{type: Number, min:0, max:1440},{type: Number, min:0, max:1440}],
+    [{type: Number, min:0, max:1440},{type: Number, min:0, max:1440}],
+  ],
   loginHistory: {type:Array},
   isAdmin: {type: Boolean, default: false, immutable: true}
 }, {
